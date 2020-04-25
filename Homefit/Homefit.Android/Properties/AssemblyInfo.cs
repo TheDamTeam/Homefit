8de6ae46a7de09f5192d45a01ce587a61b21e487
BazireJeremy <jeremybazire83@gmail.com>
2020-04-25 11:59:01 +0200

    fix start appli bug

--- Homefit/Homefit.Android/Properties/AssemblyInfo.cs
+++ Homefit/Homefit.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Homefit.Android")]
+[assembly: AssemblyTitle("Homefit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Homefit.Android")]
+[assembly: AssemblyProduct("Homefit")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
