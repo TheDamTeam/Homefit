c74076ad0130611a2d4c540d498b5d6a6ab72161
Jennifer Lobato Caldas <jennifer.caldas.goncalves@gmail.com>
2020-04-27 00:47:21 +0200

    Design DetailDefiView Finish Add Image for DefiView

--- Homefit/Homefit/ViewModels/DetailDefisViewModel.cs
+++ Homefit/Homefit/ViewModels/DetailDefisViewModel.cs
@@ -11,18 +11,14 @@ namespace Homefit.ViewModels
 {
     public class DetailDefisViewModel : BaseViewModel
     {
+        #region Properties       
         public INavigation Navigation { get; set; }
+
         private Defis defis;
         public Defis Defis
         {
             get { return defis; }
             set { SetProperty(ref defis, value); }
-        }
-        public ICommand ParticiperCommandButton => new Command(ExecuteClickedCommandAsync);
-
-        private void ExecuteClickedCommandAsync(object obj)
-        {
-            Navigation.PushAsync(new CompteurView());
         }
 
         private string description;
@@ -31,43 +27,77 @@ namespace Homefit.ViewModels
             get { return description; }
             set { SetProperty(ref description, value); }
         }
+
         private DateTime duree;
         public DateTime Duree
         {
             get { return duree; }
             set { SetProperty(ref duree, value); }
         }
+
         private int height;
         public int Height
         {
             get { return height; }
             set { SetProperty(ref height, value); }
         }
+
+        private string libelle;
+        public string Libelle
+        {
+            get { return libelle; }
+            set { SetProperty(ref libelle, value); }
+        }
+
+        private string defiName;
+        public string DefiName
+        {
+            get { return defiName; }
+            set { SetProperty(ref defiName, value); }
+        }
+
         private List<Classement> classements;
         public List<Classement> Classement
         {
             get { return classements; }
-            set 
-            { 
+            set
+            {
                 SetProperty(ref classements, value);
                 Height = (Classement.Count * 50);
             }
         }
+        #endregion
+
+        #region Commands     
+        public ICommand ParticiperCommandButton => new Command(ExecuteClickedCommandAsync);
+        #endregion
+
         public DetailDefisViewModel(Defis defisParam)
         {
             Defis = defisParam;
             GetInformationViewModel();
+            Title = $"{defis.Libelle}";
         }
+
+        #region ExecuteCommands       
+        private void ExecuteClickedCommandAsync(object obj)
+        {
+            Navigation.PushAsync(new CompteurView());
+        }
+
         private async void GetInformationViewModel()
         {
             Description = Defis.Description;
-            duree = Defis.Duree;
+            Duree = Defis.Duree;
+            Libelle = Defis.Libelle;
             var apiResponse = await App.Client.GetClassement(Defis.Id);
             Classement = apiResponse.Liste;
-            for(int i = 0;i<Classement.Count;i++)
+            for (int i = 0; i < Classement.Count; i++)
             {
                 Classement[i].Position = i + 1;
+                Classement[i].DefiName = Libelle;
             }
         }
+        #endregion
     }
 }
